c2c0deb6d66890f79697aae6cb44549a785bb4ce
fedir72 <fedir72@ua.fm>
2020-04-11 18:15:47 +0300

    a starter interfase was added

---
+++ 43_mapkit/43_mapkit/Controllers/ThirdViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ThirdViewController.swift
+//  43_mapkit
+//
+//  Created by fedir on 11.04.2020.
+//  Copyright © 2020 fedir. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ 43_mapkit/43_mapkit/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  43_mapkit
+//
+//  Created by fedir on 11.04.2020.
+//  Copyright © 2020 fedir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
